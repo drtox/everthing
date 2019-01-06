3edf6875561d5b80f4b1461ef3ba913a611f5e44
drtox <45079652+drtox@users.noreply.github.com>
2019-01-06 17:04:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,36 +1,23 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '-'
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`Start ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-	
+
+client.on('ready', function() {
+    console.log(`i am ready ${client.user.username}`);
 });
 
 
-const developers = ["507978288250093568","471044981499625514",""]
-const adminprefix = ".";
+
+
+
+
+
+
+
+
+
+const developers = ["471044981499625514","507978288250093568",""]
+const adminprefix = "-";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
@@ -63,7 +50,6 @@ if (message.content.startsWith(adminprefix + 'setavatar')) {
     message.channel.send(`Changing The Avatar To :**${argresult}** `);
 }
 });
-
 
 
 client.login(process.env.BOT_TOKEN);
